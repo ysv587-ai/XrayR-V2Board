f8d97e6dd2aabccdc8dda1090a1cd64f2dcf1b8e
ysv587-ai <60025185+ysv587-ai@users.noreply.github.com>
2022-02-05 15:32:14 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -200,7 +200,7 @@ install_XrayR() {
 
     # Writing json
     echo "正在尝试写入配置文件..."
-    wget https://cdn.jsdelivr.net/gh/missuo/XrayR-V2Board/config.yml -O /etc/XrayR/config.yml
+    wget https://cdn.jsdelivr.net/gh/ysv587-ai/XrayR-V2Board/config.yml -O /etc/XrayR/config.yml
     sed -i "s/NodeID:.*/NodeID: ${node_id}/g" /etc/XrayR/config.yml
     sed -i "s/NodeType:.*/NodeType: ${node_type}/g" /etc/XrayR/config.yml
     echo ""
@@ -241,8 +241,8 @@ install_XrayR() {
     echo "XrayR version            - 查看 XrayR 版本"
     echo "------------------------------------------"
     echo "One-Step Script Based on XrayR-Release"
-    echo "Telegram: https://t.me/missuo"
-    echo "Github: https://github.com/missuo/XrayR-V2Board"
+    echo "Telegram: https://t.me/spcloud"
+    echo "Github: https://github.com/ysv587-ai/XrayR-V2Board"
     echo "Powered by Vincent"
 }
 
